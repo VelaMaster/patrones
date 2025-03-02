4425548f42cd926aa3a4c13cb900f86d91991237
VelaMaster <pdiegovela@gmail.com>
2025-03-02 14:06:16 -0600

    auto

--- Auto.java
+++ Auto.java
@@ -21,6 +21,16 @@ public class Auto
             return new Auto(tipo, asientos);
         }
     }
+    public class Main{
+        public static void main(String args[]){
+            Auto auto1 = new Auto.ConstructorAuto().establecerTipo("A").establecerAsientos(5).construir();
+            Auto auto2 = new Auto.ConstructorAuto().establecerTipo("B").establecerAsientos(5).construir();
+            Auto auto3 = new Auto.ConstructorAuto().establecerTipo("C").establecerAsientos(5).construir();
+            auto1.mostrar();
+            auto2.mostrar();
+            auto3.mostrar();
+        }
+    }
     public void mostrar(){
         System.out.println("Tipo de auto: " + tipo + ", Asientos: " + asientos);
     }
